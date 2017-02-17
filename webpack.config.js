4c5b882812cb374c126065061fe7ba7b8168c8c5
Bob Manary <bob.manary@ampleorganics.com>
2017-02-17 11:05:00 -0500

    Initial webpack test

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var config = {
+	entry: {
+		"ui-router-marionette": ["./src/index.coffee"],
+		"ui-router-marionette.min": ["./src/index.coffee"]
+	},
+	output: {
+		path: path.resolve(__dirname, "_bundles"),
+		filename: "[name].js",
+		libraryTarget: "umd",
+		library: "UIRouterMarionette",
+		umdNamedDefine: true
+	},
+	resolve: {
+		extensions: ["", ".coffee", ".js"]
+	},
+	devtool: 'source-map',
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			minimize: true,
+			include: /\.min\.js$/,
+		})
+	],
+	module: {
+		loaders: [
+      { test: /\.coffee$/, loader: "coffee-loader" }
+		]
+	},
+	externals: {
+		"Marionette": { root: 'Marionette', amd: 'backbone.marionette', commonjs2: 'backbone.marionette', commonjs: 'backbone.marionette' }
+	}
+};
+
+module.exports = config;
